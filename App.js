37769c0d067448dbddc5ae645c656641e40153eb
Arittri Saha <70344523+Arittri@users.noreply.github.com>
2022-11-11 00:23:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar.js';
+import Todo from './Todo.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Todo  />
+    </div>
+  );
+}
+
+export default App;
